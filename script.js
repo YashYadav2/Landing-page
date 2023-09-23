81409bb2cf96df86a0bf2552ed0aa7af7e6af6c1
Yashpal Yadav <127642524+YashYadav2@users.noreply.github.com>
2023-09-23 09:45:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// JavaScript code to add functionality to the landing page
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    const button = document.getElementById("clickMe");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+        alert("Button Clicked!");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    const button = document.getElementById("learn");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+        alert("Button Clicked! for Web Development Course");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    const button = document.getElementById("learn1");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+        alert("Button Clicked! for Data Structure & Algorithms Course");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button element
+    const button = document.getElementById("learn2");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+        alert("Button Clicked! for Cyber Security Course");
+    });
+});
